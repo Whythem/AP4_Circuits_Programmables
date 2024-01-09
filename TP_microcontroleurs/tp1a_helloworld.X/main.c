8b91770cb9e14cc3533560b7b61e46a5fe88ecc1
Whythem <mathiasmilhec@gmail.com>
2024-01-09 12:29:10 +0100

    exo1a

--- TP_microcontroleurs/tp1a_helloworld.X/main.c
+++ TP_microcontroleurs/tp1a_helloworld.X/main.c
@@ -9,10 +9,41 @@
 #include "configbits.h" // Bits de configuration
 #include <xc.h>         // Definition des registres specifiques au uC
 
+#define DIR_LED1 TRISDbits.TRISD0
+#define DIR_LED2 TRISDbits.TRISD1
+#define DIR_LED3 TRISDbits.TRISD2
+#define DIR_LED4 TRISDbits.TRISD3
+#define DIR_LED5 TRISBbits.TRISB0
+#define DIR_LED6 TRISBbits.TRISB1
+#define DIR_LED7 TRISBbits.TRISB2
+#define DIR_LED8 TRISBbits.TRISB3
+
+const int delay_cycles = 500; // NB of delay cycles
+
+void init_leds() {
+    DIR_LED1 = 0;
+    DIR_LED2 = 0;
+    DIR_LED3 = 0;
+    DIR_LED4 = 0;
+    DIR_LED5 = 0;
+    DIR_LED6 = 0;
+    DIR_LED7 = 0;
+    DIR_LED8 = 0;
+}
+
+void delay_approx() {
+    for(int i=0; i<delay_cycles; i++){}
+}
+
 void main(void) {
     /* Code d'initialisation */
-
+    
     while(1){
-        /* Code a executer dans une boucle infinie */
+        PORTD |= 15;
+        PORTB &= 239;
+        delay_approx();
+        PORTB |= 15;
+        PORTD &= 239;
+        delay_approx();
     }
 }
