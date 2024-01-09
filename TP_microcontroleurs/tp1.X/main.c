5c7242233e2112a1cfb50f483b23f7ef967102d0
Whythem <mathiasmilhec@gmail.com>
2024-01-09 11:21:54 +0100

    Correction

---
+++ TP_microcontroleurs/tp1.X/main.c
@@ -0,0 +1,17 @@
+/*
+ * File:   main.c
+ * Author: mathias & Thibaut
+ *
+ * Created on 9 janvier 2024, 11:19
+ */
+
+#include "configbits.h" // Bits de configuration
+#include <xc.h>         // Definition des registres specifiques au uC
+
+void main(void) {
+    /* Code d'initialisation */
+
+    while(1){
+        /* Code a executer dans une boucle infinie */
+    }
+}
