8f416a9678604723e552da1edea6c4ad53ac538f
Whythem <mathiasmilhec@gmail.com>
2024-01-09 11:45:04 +0100

    correction tp1a

---
+++ TP_microcontroleurs/tp1a_helloworld.X/main.c
@@ -0,0 +1,18 @@
+/*
+ * File:   main.c
+ * Author: mathias & thibaut
+ *
+ * Created on 9 janvier 2024, 11:44
+ */
+
+
+#include "configbits.h" // Bits de configuration
+#include <xc.h>         // Definition des registres specifiques au uC
+
+void main(void) {
+    /* Code d'initialisation */
+
+    while(1){
+        /* Code a executer dans une boucle infinie */
+    }
+}
