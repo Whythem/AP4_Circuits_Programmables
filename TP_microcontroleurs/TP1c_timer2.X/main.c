546393706c15081951c26c449b51dbb5f65c8554
Whythem <mathiasmilhec@gmail.com>
2024-01-22 09:26:04 +0100

    tp1c start

---
+++ TP_microcontroleurs/TP1c_timer2.X/main.c
@@ -0,0 +1,38 @@
+/*
+ * File:   main.c
+ * Author: mathias & thibaut
+ *
+ * Created on 9 janvier 2024, 13:40
+ */
+
+
+#include "configbits.h" // Bits de configuration
+#include <xc.h>         // Definition des registres specifiques au uC
+
+#define DIR_LED1 TRISDbits.TRISD0
+#define DIR_LED2 TRISDbits.TRISD1
+#define DIR_LED3 TRISDbits.TRISD2
+#define DIR_LED4 TRISDbits.TRISD3
+#define DIR_LED5 TRISBbits.TRISB0
+#define DIR_LED6 TRISBbits.TRISB1
+#define DIR_LED7 TRISBbits.TRISB2
+#define DIR_LED8 TRISBbits.TRISB3
+
+void init_leds() {
+    DIR_LED1 = 0;
+    DIR_LED2 = 0;
+    DIR_LED3 = 0;
+    DIR_LED4 = 0;
+    DIR_LED5 = 0;
+    DIR_LED6 = 0;
+    DIR_LED7 = 0;
+    DIR_LED8 = 0;
+}
+void main(void) {
+    /* Code d'initialisation */
+    init_leds()
+
+    while(1){
+        /* Code a executer dans une boucle infinie */
+    }
+}
