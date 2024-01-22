501554593866556a2ec228619499f85d0bfec5e7
Whythem <mathiasmilhec@gmail.com>
2024-01-22 10:22:50 +0100

    tp1c

--- TP_microcontroleurs/TP1c_timer2.X/main.c
+++ TP_microcontroleurs/TP1c_timer2.X/main.c
@@ -28,11 +28,48 @@ void init_leds() {
     DIR_LED7 = 0;
     DIR_LED8 = 0;
 }
+
+void delay_timer2() {
+    for (int i = 0; i <= 500; i++) {
+        while(PIR1bits.TMR2IF == 0) {    
+        }
+        PIR1bits.TMR2IF = 0; 
+    }
+}
+
+
 void main(void) {
     /* Code d'initialisation */
-    init_leds()
-
+    init_leds();
+    T2CONbits.TMR2ON = 1;
+    T2CONbits.T2OUTPS = 0;
+    T2CONbits.T2CKPS = 2;
+    PR2 = 249;
     while(1){
         /* Code a executer dans une boucle infinie */
+        DIR_LED1 = 1;
+        delay_timer2();
+        DIR_LED1 = 0;
+        DIR_LED2 = 1;
+        delay_timer2();
+        DIR_LED2 = 0;
+        DIR_LED3 = 1;
+        delay_timer2();
+        DIR_LED3 = 0;
+        DIR_LED4 = 1;
+        delay_timer2();
+        DIR_LED4 = 0;
+        DIR_LED5 = 1;
+        delay_timer2();
+        DIR_LED5 = 0;
+        DIR_LED6 = 1;
+        delay_timer2();
+        DIR_LED6 = 0;
+        DIR_LED7 = 1;
+        delay_timer2();
+        DIR_LED7 = 0;
+        DIR_LED8 = 1;
+        delay_timer2();
+        DIR_LED8 = 0;
     }
 }
