9d2442c1672765e569ef46b92db5b52edfb38a4b
Whythem <mathiasmilhec@gmail.com>
2024-01-23 17:16:24 +0100

    TP2d

---
+++ TP_microcontroleurs/TP2d_breathing.X/main.c
@@ -0,0 +1,54 @@
+/*
+ * File:   main.c
+ * Author: mathias & thibaut
+ *
+ * Created on 9 janvier 2024, 13:40
+ */
+
+
+#include "configbits.h" // Bits de configuration
+#include <xc.h>         // Definition des registres specifiques au uC
+
+#define DIR_LED1 TRISDbits.TRISD0
+
+#define LED1 PORTDbits.RD0
+
+void init() {
+    DIR_LED1 = 0;
+
+    ANSELBbits.ANSB0 = 0;
+    
+    //CCPTMRSbits.P4TSEL = 0;  // Met sur timer 2 PWM4 page 289
+    PWM4CONbits.PWM4EN = 1;  // Active PWM module
+    
+    //Init timer2
+    T2CONbits.TMR2ON = 1;
+    T2CONbits.T2OUTPS = 1;
+    T2CONbits.T2CKPS = 2;
+    PR2 = 244;
+}
+
+void main(void) {
+    /* Code d'initialisation */
+    init();
+    
+    
+    while(1){
+        for (int i = 0; i < 1024; i++) {
+            PWM4DCH = i / 4;
+            PWM4DCLbits.PWM4DCL = i % 4;
+            while(PIR1bits.TMR2IF == 0);
+            PIR1bits.TMR2IF = 0;
+            RD0PPSbits.RD0PPS = 0x0F;
+        }
+        for (int i = 1024; i > 0; i--) {
+            PWM4DCH = i / 4;
+            PWM4DCLbits.PWM4DCL = i % 4;
+            while(PIR1bits.TMR2IF == 0);
+            PIR1bits.TMR2IF = 0; 
+            RD0PPSbits.RD0PPS = 0x0F;
+        }
+        
+        RD0PPSbits.RD0PPS = 0x0F;
+    }
+}
