0fd594b8577237c626d9e9d4963af05586b022af
Whythem <mathiasmilhec@gmail.com>
2024-01-23 17:26:56 +0100

    TPd end

--- TP_microcontroleurs/TP2d_breathing.X/main.c
+++ TP_microcontroleurs/TP2d_breathing.X/main.c
@@ -20,11 +20,12 @@ void init() {
     
     //CCPTMRSbits.P4TSEL = 0;  // Met sur timer 2 PWM4 page 289
     PWM4CONbits.PWM4EN = 1;  // Active PWM module
+    RD0PPSbits.RD0PPS = 0x0F;
     
     //Init timer2
     T2CONbits.TMR2ON = 1;
-    T2CONbits.T2OUTPS = 1;
-    T2CONbits.T2CKPS = 2;
+    T2CONbits.T2OUTPS = 7;
+    T2CONbits.T2CKPS = 0;
     PR2 = 244;
 }
 
@@ -35,20 +36,16 @@ void main(void) {
     
     while(1){
         for (int i = 0; i < 1024; i++) {
-            PWM4DCH = i / 4;
-            PWM4DCLbits.PWM4DCL = i % 4;
+            PWM4DCH = i >> 2;
+            PWM4DCLbits.PWM4DCL = i &3;
             while(PIR1bits.TMR2IF == 0);
             PIR1bits.TMR2IF = 0;
-            RD0PPSbits.RD0PPS = 0x0F;
         }
         for (int i = 1024; i > 0; i--) {
-            PWM4DCH = i / 4;
-            PWM4DCLbits.PWM4DCL = i % 4;
+            PWM4DCH = i >> 2;
+            PWM4DCLbits.PWM4DCL = i &3;
             while(PIR1bits.TMR2IF == 0);
             PIR1bits.TMR2IF = 0; 
-            RD0PPSbits.RD0PPS = 0x0F;
         }
-        
-        RD0PPSbits.RD0PPS = 0x0F;
     }
 }
