92e21b7c86d32ed1992eb5bedb1ce3ea7b358e37
Whythem <mathiasmilhec@gmail.com>
2024-02-02 12:14:02 +0100

    TP3c_uart_echo

--- TP_microcontroleurs/TP3b_uart_echo.X/main.c
+++ TP_microcontroleurs/TP3b_uart_echo.X/main.c
@@ -11,7 +11,7 @@
 
 void init() {
     RC6PPSbits.RC6PPS = 20; // Connexion RC6 à TX/CK  p 154.
-    RXPPSbits.RXPPS = 31; //Connexion RC7 à RXPPS p.153
+    RXPPSbits.RXPPS = 23; //Connexion RC7 à RXPPS p.153
     
     TRISCbits.TRISC6 = 0; //sortie
     TRISCbits.TRISC7 = 1; // entrée
@@ -23,24 +23,23 @@ void init() {
     SP1BRGHbits.SP1BRGH = 0; // Octet haut
     SP1BRGLbits.SP1BRGL = 51; // Octet bas
     
-    RC1STAbits.SPEN = 1; //Activation pins
     TX1STAbits.TXEN = 1; // Activation transmitter
     RC1STAbits.CREN = 1; //Activation receiver
+    RC1STAbits.SPEN = 1; //Activation pins
+    
+    INTCONbits.GIE = 1; // Enable all interrupts
+    INTCONbits.PEIE = 1; // enable peripherals interupts
+    PIE1bits.RCIE = 1;  // enable the USART receive interrupt
 }
 
-void delay() {
-    while(PIR1bits.TMR2IF == 0) {    
-    }
-    PIR1bits.TMR2IF = 0; 
+void __interrupt() isr(void) {
+    TX1REG = RC1REG;
 }
 
 void main(void) {
     /* Code d'initialisation */
     init();
+    while(1) {
     
-    T2CONbits.TMR2ON = 1;
-    T2CONbits.T2OUTPS = 7;
-    T2CONbits.T2CKPS = 0;
-    
-
+    }
 }
