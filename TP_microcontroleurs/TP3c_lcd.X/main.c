89dd29ba921b07b0f88b5a72f66befefce8ea380
Whythem <mathiasmilhec@gmail.com>
2024-02-02 12:19:16 +0100

    INIT TP3c_lcd

---
+++ TP_microcontroleurs/TP3c_lcd.X/main.c
@@ -0,0 +1,19 @@
+/*
+ * File:   main.c
+ * Author: mathias & thibaut
+ *
+ * Created on 9 janvier 2024, 13:40
+ */
+
+
+#include "configbits.h" // Bits de configuration
+#include <xc.h>         // Definition des registres specifiques au uC
+
+
+void main(void) {
+    /* Code d'initialisation */
+
+    while(1) {
+    
+    }
+}
