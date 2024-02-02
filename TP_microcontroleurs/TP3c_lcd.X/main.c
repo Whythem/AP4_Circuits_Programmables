8f2e74dc3dbc900d64c2f6c0d055cd0cb8341367
Whythem <mathiasmilhec@gmail.com>
2024-02-02 12:24:40 +0100

    TP3c_lcd

--- TP_microcontroleurs/TP3c_lcd.X/main.c
+++ TP_microcontroleurs/TP3c_lcd.X/main.c
@@ -8,11 +8,19 @@
 
 #include "configbits.h" // Bits de configuration
 #include <xc.h>         // Definition des registres specifiques au uC
-
+#include "lcd.h"
+#include "spi.h"
 
 void main(void) {
     /* Code d'initialisation */
-
+    
+    SPI_InitializePins();
+    LCD_InitializePins();
+    SPI_Initialize();
+    LCD_Initialize();
+    LCD_Clear();
+    LCD_GoTo(0,0);
+    LCD_WriteString("Hello World !");
     while(1) {
     
     }
